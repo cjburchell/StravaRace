35573e5a6eacfc0e18d65ffc5ed958760e9e7912
cjburchell <cjburchell@yahoo.com>
2016-08-22 21:04:17 -0400

    now saving the user name in the database

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -2,12 +2,13 @@
  * Created by Christiaan on 2016-08-12.
  */
 (function(exports){
-    exports.User = function User(athleteId) {
+    exports.User = function User(athleteId, name) {
         this._id = undefined;
         this._rev = undefined;
         this.type = "user";
         this.athleteId = athleteId;
         this.role = "user";
         this.maxActiveActivities = 3;
+        this.name = name;
     };
 })(typeof exports === 'undefined'? this['user']={}: exports);
